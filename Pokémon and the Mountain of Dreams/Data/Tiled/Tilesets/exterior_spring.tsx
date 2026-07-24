4b8e018e6560883af1abb22a802f250d2a154b44
MarcosJGA <87387309+CaeLock@users.noreply.github.com>
2026-07-25 01:04:23 +0200

    Test map

---
+++ Pokémon and the Mountain of Dreams/Data/Tiled/Tilesets/exterior_spring.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="exterior_spring" tilewidth="32" tileheight="32" tilecount="1256" columns="8">
+ <image source="../Assets/exterior-spring.png" width="256" height="5024"/>
+</tileset>

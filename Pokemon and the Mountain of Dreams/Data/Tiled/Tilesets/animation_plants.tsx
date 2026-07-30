70f1df7fe49cdc69686f17aeae868ff5e0df0e3f
MarcosJGA <87387309+CaeLock@users.noreply.github.com>
2026-07-30 20:31:12 +0200

    First map of the game and events

---
+++ Pokemon and the Mountain of Dreams/Data/Tiled/Tilesets/animation_plants.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="animation_plants" tilewidth="32" tileheight="32" tilecount="234" columns="26">
+ <image source="../Assets/plants_animations.png" width="850" height="300"/>
+ <tile id="27">
+  <animation>
+   <frame tileid="27" duration="200"/>
+   <frame tileid="28" duration="200"/>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="30" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="200"/>
+   <frame tileid="33" duration="200"/>
+   <frame tileid="34" duration="200"/>
+   <frame tileid="35" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="37">
+  <animation>
+   <frame tileid="37" duration="200"/>
+   <frame tileid="38" duration="200"/>
+   <frame tileid="39" duration="200"/>
+   <frame tileid="40" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="42">
+  <animation>
+   <frame tileid="42" duration="200"/>
+   <frame tileid="43" duration="200"/>
+   <frame tileid="44" duration="200"/>
+   <frame tileid="45" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <animation>
+   <frame tileid="47" duration="200"/>
+   <frame tileid="48" duration="200"/>
+   <frame tileid="49" duration="200"/>
+   <frame tileid="50" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="79">
+  <animation>
+   <frame tileid="79" duration="200"/>
+   <frame tileid="105" duration="200"/>
+   <frame tileid="131" duration="200"/>
+   <frame tileid="157" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <animation>
+   <frame tileid="84" duration="200"/>
+   <frame tileid="110" duration="200"/>
+   <frame tileid="136" duration="200"/>
+   <frame tileid="162" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="89">
+  <animation>
+   <frame tileid="89" duration="200"/>
+   <frame tileid="115" duration="200"/>
+   <frame tileid="141" duration="200"/>
+   <frame tileid="167" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="94">
+  <animation>
+   <frame tileid="94" duration="200"/>
+   <frame tileid="120" duration="200"/>
+   <frame tileid="146" duration="200"/>
+   <frame tileid="172" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="99">
+  <animation>
+   <frame tileid="99" duration="200"/>
+   <frame tileid="125" duration="200"/>
+   <frame tileid="151" duration="200"/>
+   <frame tileid="177" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
